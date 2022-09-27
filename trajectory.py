76e9e0ce5bf85966732ee70ad48a60f1a631300e
Jingyan Zhang <105741006+jingaling00@users.noreply.github.com>
2022-09-27 17:53:06 -0400

    Project A Submission

--- trajectory.py
+++ trajectory.py
@@ -4,26 +4,11 @@ Created on Tue Sep 20 19:54:11 2022
 
 @author: jingy
 """
-
-# -*- coding: utf-8 -*-
-"""
-Template code
-
-
-1) Get the user inputs
-2) Generate a window with the appropriate size based on the user inputs
-3) Place the initial circle (ball) in the window as described in the instruction
-4) Run the simulation and check when the ball hits the horizontal edge. Make the necessary adjustment
-5) Termiunate the simulation when the stopping criteria is reached.
-
-
-
-"""
 from graphics import *
 import time
 import math
 
-""" Write your code here """
+""" A simple code to animate a yellow ball's projectile motion """
 
 G = 9.8 # gravity
 
@@ -69,26 +54,25 @@ def trajectory(v0, theta, radius, time_incr):
         if iteration_counter > 0: 
             print(f'iteration {iteration_counter} , dx = {dx:.3f}, dy = {dy:.3f}')
         iteration_counter+=1
-    print('done')
         
 if __name__ == '__main__':
     # User input for radius
     radius = int(input('Enter Radius (a number between 10 and 20 inclusive): '))
-    while radius > 20 or radius < 10:
+    while radius > 20.0 or radius < 10.0:
         print('\nError. Please enter radius between 10 and 20, inclusive.')
         radius = int(input('Enter Radius (a number between 10 and 20 inclusive): '))
         
     # User input for initial speed
     v0 = int(input('Enter initial speed (a number between 30 and 80 inclusive): '))
-    while v0 > 80 or v0 < 30:
+    while v0 > 80.0 or v0 < 30.0:
         print(f'\nError. Please enter initial speed between 30 and 80, inclusive.')
         v0 = int(input('Enter initial speed (a number between 30 and 80 inclusive): '))
     
     # User input for initial angle
     theta = int(input('Enter initial trajectory angle in degrees (30-90 inclusive): '))
-    while theta > 90 or theta < 30:
+    while theta > 90.0 or theta < 30.0:
         print(f'\nError. Please enter initial angle between 30 and 90 degrees, inclusive.')
-        theta = int(input('Enter initial trajectory angle in degrees (30-90 inclusive)": '))
+        theta = int(input('Enter initial trajectory angle in degrees (30-90 inclusive): '))
     
     # User input for time increment
     time_incr = float(input('Enter a value for the simulation time increment: '))
